bb8a40f4e8834afe7a20f7446a74d1e97e2777e4
Anderson Lobo <anderson96x@gmail.com>
2025-06-26 23:41:42 -0300

    wip: setting scenario and centering container

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 // TODO
 // Make it go quicker and quicker (smaller interval)
 
-
-let windowWidth = window.innerWidth - 200;
-let windowHeight = window.innerHeight - 200;
-
-// Adjusts according to user viewport
-window.onresize = function() {
-    windowWidth = window.innerWidth - 200;
-    windowHeight = window.innerHeight - 200;
-}
-
 function createAlien() {
     if (document.getElementById('createdAlien')) {
         document.getElementById('createdAlien').remove();
     }
 
+    let container = document.querySelector('.game-container');
+    let containerMarginLeft = window.getComputedStyle(container).marginLeft;
+
+    let windowWidth = container.offsetWidth - 200;
+    let windowHeight = container.offsetHeight - 200;
+
     let alien = document.createElement('img');
     let alienPick = Math.floor(Math.random() * 8) + 1;
+
     let alienSize = Math.floor(Math.random() * 150) + 50; // 50px to 200px
     let alienMirror = Math.random() < 0.5 ? -1 : 1; // Since i can't use 0 with ScaleX, If less than 0.5 it gives -1
-    let alienPosX = Math.floor(Math.random() * windowWidth);
+
+    let alienPosX = Math.floor(Math.random() * windowWidth) + containerMarginLeft;
     let alienPosY = Math.floor(Math.random() * windowHeight);
+    
 
     alien.src = './assets/alien' + alienPick + '.png';
-    document.body.appendChild(alien);
+    container.appendChild(alien);
 
     alien.id = 'createdAlien';
     alien.style.position = 'absolute';
@@ -34,12 +33,15 @@ function createAlien() {
     alien.style.transform = 'scaleX(' + alienMirror + ')';
     
     // Debug boy
-    console.log('alienSize ' + alienSize);
-    console.log('alienMirror ' + alienMirror);
-    console.log('windowWidth ' + windowWidth);
-    console.log('windowHeight ' + windowHeight);
-    console.log('alienPosX ' + alienPosX);
-    console.log('alienPosY ' + alienPosY);
+    // console.log('alienSize ' + alienSize);
+    // console.log('alienMirror ' + alienMirror);
+    // console.log('windowWidth ' + windowWidth);
+    // console.log('windowHeight ' + windowHeight);
+    // console.log('alienPosX ' + alienPosX);
+    // console.log('alienPosY ' + alienPosY);
+    // console.log(container.offsetWidth);
+    // console.log(container.offsetHeight);
+    console.log(window.getComputedStyle(container).marginLeft)
 }
 
 setInterval(createAlien, 1000);
