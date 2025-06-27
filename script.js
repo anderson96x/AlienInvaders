9e3b660594b8c7cb25cce33e6b94fca85afbee9a
Anderson Lobo <anderson96x@gmail.com>
2025-06-26 21:08:44 -0300

    feat: random alien size

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let windowWidth = window.innerWidth - 100;
-let windowHeight = window.innerHeight - 100;
+let windowWidth = window.innerWidth - 200;
+let windowHeight = window.innerHeight - 200;
 
 // Adjusts according to user viewport
 window.onresize = function() {
-    windowWidth = window.innerWidth - 100;
-    windowHeight = window.innerHeight - 100;
+    windowWidth = window.innerWidth - 200;
+    windowHeight = window.innerHeight - 200;
 }
 
 let alienPosX = Math.floor(Math.random() * windowWidth);
@@ -13,6 +13,8 @@ let alienPosY = Math.floor(Math.random() * windowHeight);
 function createAlien() {
     let alien = document.createElement('img');
     let alienPick = Math.floor(Math.random() * 8) + 1;
+    let alienSize = Math.floor(Math.random() * 150) + 50; // 50px to 200px
+
     alien.src = './assets/alien' + alienPick + '.png';
     alien.className = 'alien';
     document.body.appendChild(alien);
@@ -20,13 +22,15 @@ function createAlien() {
     alien.style.position = 'absolute';
     alien.style.left = alienPosX + 'px';
     alien.style.top = alienPosY + 'px';
+    alien.style.width = alienSize + 'px';
+    console.log('alienSize ' + alienSize);
 }
 
 createAlien();
 
 
 
-
+// Debug boy
 console.log('windowWidth ' + windowWidth);
 console.log('windowHeight ' + windowHeight);
 console.log('alienPosX ' + alienPosX);
