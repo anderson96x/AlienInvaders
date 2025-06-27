e066391c57b01503f88ffb6e1b1874b6905c76ce
Anderson Lobo <anderson96x@gmail.com>
2025-06-26 22:09:31 -0300

    feat: creating and removing alien

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// TODO
+// Make it go quicker and quicker (smaller interval)
+
+
 let windowWidth = window.innerWidth - 200;
 let windowHeight = window.innerHeight - 200;
 
@@ -7,19 +11,22 @@ window.onresize = function() {
     windowHeight = window.innerHeight - 200;
 }
 
-let alienPosX = Math.floor(Math.random() * windowWidth);
-let alienPosY = Math.floor(Math.random() * windowHeight);
-
 function createAlien() {
+    if (document.getElementById('createdAlien')) {
+        document.getElementById('createdAlien').remove();
+    }
+
     let alien = document.createElement('img');
     let alienPick = Math.floor(Math.random() * 8) + 1;
     let alienSize = Math.floor(Math.random() * 150) + 50; // 50px to 200px
     let alienMirror = Math.random() < 0.5 ? -1 : 1; // Since i can't use 0 with ScaleX, If less than 0.5 it gives -1
+    let alienPosX = Math.floor(Math.random() * windowWidth);
+    let alienPosY = Math.floor(Math.random() * windowHeight);
 
     alien.src = './assets/alien' + alienPick + '.png';
-    alien.className = 'alien';
     document.body.appendChild(alien);
 
+    alien.id = 'createdAlien';
     alien.style.position = 'absolute';
     alien.style.left = alienPosX + 'px';
     alien.style.top = alienPosY + 'px';
@@ -29,14 +36,14 @@ function createAlien() {
     // Debug boy
     console.log('alienSize ' + alienSize);
     console.log('alienMirror ' + alienMirror);
+    console.log('windowWidth ' + windowWidth);
+    console.log('windowHeight ' + windowHeight);
+    console.log('alienPosX ' + alienPosX);
+    console.log('alienPosY ' + alienPosY);
 }
 
-createAlien();
+setInterval(createAlien, 1000);
 
 
 
-// Debug boy
-console.log('windowWidth ' + windowWidth);
-console.log('windowHeight ' + windowHeight);
-console.log('alienPosX ' + alienPosX);
-console.log('alienPosY ' + alienPosY);
+
